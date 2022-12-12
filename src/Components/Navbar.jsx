4b7a4a08e4bb47570b2d60bea497cf63139d55b3
pramodkumarsharma0754 <sharmapramod1994@gmail.com>
2022-12-12 23:22:57 +0530

    port

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../Style/Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+     <div>
+       <button>Home</button>
+     </div>
+     <div>
+       <button>Skills</button>
+     </div>
+     <div>
+       <button>Project</button>
+     </div>
+     <div>
+       <button>Resume</button>
+     </div>
+     <div>
+       <button>Contact</button>
+     </div>
+    </div>
+  )
+}
+
+export default Navbar
